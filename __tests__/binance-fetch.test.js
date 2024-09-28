5a9edf96aaa125fed225a6968f9f77ae7709d2a2
ltftf <ltftf4@gmail.com>
2024-09-29 00:08:25 +0300

    test fixes

--- __tests__/binance-fetch.test.js
+++ __tests__/binance-fetch.test.js
@@ -1,6 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { exec } from "child_process";
-import { promisify } from "util";
+import { spawn } from "child_process";
 import { join } from "path";
 import { Tests } from "./binance-fetch-tests-list.js";
 import chokidar from "chokidar";
@@ -17,30 +16,37 @@ beforeAll(async () => {
   }
 });
 
-const execPromise = promisify(exec);
-
 describe.concurrent(
   "Ensure that data for each data type of each product is accessed correctly",
   async () => {
-    function run(dir, product, dataType, date, symbol, interval) {
-      execPromise(
-        `node ${join(
-          import.meta.dirname,
-          "..",
-          "bin",
-          "binance-fetch.js"
-        )} --date ${date} --product ${product} --data-type ${dataType} --symbols ${symbol} --output-path "${join(
-          resultDir,
-          dir
-        )}"${interval ? " --intervals " + interval : ""}`
-      );
-    }
     const watcher = chokidar.watch(resultDir, { awaitWriteFinish: false });
     for (const test of Tests) {
       const endpoint = `${test.product}_${test.dataType}_${
         test.date.length === 7 ? "monthly" : "daily"
       }`;
       it(endpoint, async () => {
+        let proc;
+        function run(dir, product, dataType, date, symbol, interval = "1h") {
+          proc = spawn(
+            "node",
+            [
+              join(import.meta.dirname, "..", "bin", "binance-fetch.js"),
+              "--date",
+              date,
+              "--product",
+              product,
+              "--data-type",
+              dataType,
+              "--symbols",
+              symbol,
+              "--intervals",
+              interval,
+              "--output-path",
+              join(resultDir, dir),
+            ],
+            { killSignal: "SIGKILL" }
+          );
+        }
         const expectedFilePath = new RegExp(
           join(
             resultDir,
@@ -54,25 +60,24 @@ describe.concurrent(
         );
         const waitForFile = new Promise((resolve) => {
           watcher.on("change", async (path) => {
-            if (path) {
-              if (expectedFilePath.test(path)) {
-                try {
-                  const file = await fs.readFile(path);
-                  if (file.length >= 4) {
-                    if (
-                      /**
-                       * verify file signature (should be a ZIP)
-                       * zip signature - '\x50\x4b\x03\x04'
-                       */
-                      Buffer.from([80, 75, 3, 4]).equals(file.subarray(0, 4))
-                    ) {
-                      resolve(true);
-                    } else {
-                      resolve(false);
-                    }
+            if (expectedFilePath.test(path)) {
+              try {
+                const file = await fs.readFile(path);
+                if (file.length >= 4) {
+                  proc.kill();
+                  if (
+                    /**
+                     * verify file signature (should be a ZIP)
+                     * zip signature - '\x50\x4b\x03\x04'
+                     */
+                    Buffer.from([80, 75, 3, 4]).equals(file.subarray(0, 4))
+                  ) {
+                    resolve(true);
+                  } else {
+                    resolve(false);
                   }
-                } catch {}
-              }
+                }
+              } catch {}
             }
           });
         });
